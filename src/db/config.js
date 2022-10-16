f812e3c1a86ade121997e7adde911887622e0761
Alina Lemishko <aliska.nofear@gmail.com>
2022-10-16 20:49:16 +0300

    added docker-compose

--- src/db/config.js
+++ src/db/config.js
@@ -1,7 +1,12 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-exports.sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-  host: process.env.MYSQL_HOSTS,
-  dialect: 'mysql',
-  logging: false // console.log,
-});
+exports.sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOSTS,
+    dialect: "mysql",
+    logging: false, // console.log,
+  }
+);
